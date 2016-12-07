db483355ed9d41441a5038fcffecec170d1e8630
Michael Edgar <t00194492@365s.ittralee.ie>
2016-12-07 14:34:15 +0000

    DoubleJump

--- RedRidingHood/Red Riding Hood/Assets/Scripts/Movement.cs
+++ RedRidingHood/Red Riding Hood/Assets/Scripts/Movement.cs
@@ -3,20 +3,38 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 
-	public float moveSpeed;
-	public float jumpHeight;
+	public float moveSpeed, jumpHeight, groundCheckRadius;
+	public Transform groundCheck;
+	public LayerMask whatIsGround;
+	private bool grounded, doubleJumped;
 
 	// Use this for initialization
 	void Start () {
 	
 	}
+
+	void FixedUpdate () 
+	{
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundCheckRadius, whatIsGround);
+	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (grounded)
+			doubleJumped = false;
 	
-		if (Input.GetKeyDown (KeyCode.Space)) 
+		if (Input.GetKeyDown (KeyCode.Space) && grounded) 
 		{
-			GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, jumpHeight);
+			//GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, jumpHeight);
+			Jump();
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space) && !doubleJumped && !grounded) 
+		{
+			//GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, jumpHeight);
+			Jump();
+			doubleJumped = true; 
 		}
 
 		if (Input.GetKey (KeyCode.D)) 
@@ -28,5 +46,10 @@ public class Movement : MonoBehaviour {
 		{
 			GetComponent<Rigidbody2D> ().velocity = new Vector2 (-moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
 		}
+	}
+
+	public void Jump()
+	{
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, jumpHeight);
 	}
 }

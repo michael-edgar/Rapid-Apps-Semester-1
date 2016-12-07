c90d752d509198357c2c854768e7d7c0e48f2ac7
Michael-Edgar <michael.edgar@students.ittralee.ie>
2016-12-07 20:49:20 +0000

    Most of the Game

---
+++ RedRidingHood/Red Riding Hood/Assets/Scripts/Bacon.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bacon : MonoBehaviour {
+
+	private float baconCount=0;
+	public LevelManager levelManager;
+	public GameObject nextCheckpoint, currentCheckpoint;
+	public Transform Player;
+
+	// Use this for initialization
+	void Start () 
+	{
+		levelManager = FindObjectOfType<LevelManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (baconCount == 3) 
+		{
+			Player.transform.position = nextCheckpoint.transform.position;
+			baconCount = 0;
+			currentCheckpoint = nextCheckpoint;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.name == "Player") 
+		{
+			baconCount= (baconCount + 1);
+		}
+	}
+
+}
---
+++ RedRidingHood/Red Riding Hood/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public GameObject currentCheckpoint;
+
+	private Movement player;
+
+	// Use this for initialization
+	void Start () {
+		player = FindObjectOfType<Movement>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void RespawnPlayer()
+	{
+		Debug.Log ("Player Respawn");
+		player.transform.position = currentCheckpoint.transform.position;
+	}
+}
